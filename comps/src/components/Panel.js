37ce93956bb2af97e7daf0a43fa9e8da9e228e78
lavanya-k27 <lavanyakannan2710@gmail.com>
2025-06-12 22:40:38 +0530

    dropdown

--- comps/src/components/Panel.js
+++ comps/src/components/Panel.js
@@ -1,12 +1,10 @@
 import classNames from "classnames"
 
-function Panel (children, className, ...rest){
+function Panel ({children, className, ...rest}){
 
-   const finalClassName =classNames("border rounded p-3 shadow bg-white w-full", className)
+   const finalClassNames = classNames("border rounded p-3 shadow bg-white w-full", className)
 
-  return(
-    <div {...rest} className={finalClassName}>{children}</div>
-  ) 
+  return <div {...rest} className={finalClassNames}>{children}</div>
 }
 
 export default Panel
