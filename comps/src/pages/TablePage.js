d1efd9d4b5968796a5cdd9abbb6b9f3d561b4e7a
lavanya-k27 <lavanyakannan2710@gmail.com>
2025-06-23 14:36:21 +0530

    custom hook

--- comps/src/pages/TablePage.js
+++ comps/src/pages/TablePage.js
@@ -1,48 +1,59 @@
-import SortableTable from "../components/SortableTable"
+import SortableTable from "../components/SortableTable";
 
-function TablePage(){
-
-  const data= [
+function TablePage() {
+  const data = [
     {
-      name:"Orange", color:"bg-orange-500", score:5
+      name: "Orange",
+      color: "bg-orange-500",
+      score: 5,
     },
     {
-      name:"Apple", color:"bg-red-500", score:3
-    },{
-      name:"Banana", color:"bg-yellow-300", score:2
-    },{
-      name:"Lime", color:"bg-green-500", score:1
-    },{
-      name:"Blueberry", color:"bg-blue-500", score:4
+      name: "Apple",
+      color: "bg-red-500",
+      score: 3,
     },
-  ]
+    {
+      name: "Banana",
+      color: "bg-yellow-300",
+      score: 2,
+    },
+    {
+      name: "Lime",
+      color: "bg-green-500",
+      score: 1,
+    },
+    {
+      name: "Blueberry",
+      color: "bg-blue-500",
+      score: 4,
+    },
+  ];
 
   const config = [
     {
-      label:"Name",
-      render: (fruit)=> fruit.name,
-      sortValue: (fruit) => fruit.name
+      label: "Name",
+      render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name,
     },
     {
-      label:"Color",
-      render: (fruit) => <div className={`p-2 m-2 ${fruit.color}`}></div>
+      label: "Color",
+      render: (fruit) => <div className={`p-2 m-2 ${fruit.color}`}></div>,
     },
     {
-      label:"Score",
+      label: "Score",
       render: (fruit) => fruit.score,
-      sortValue: (fruit) => fruit.score
-    }
-  ]
+      sortValue: (fruit) => fruit.score,
+    },
+  ];
 
-  const keyFn = (fruit) =>{
-    return fruit.name
-  }
- return(
-  <div>
-    <SortableTable data={data} config={config} keyFn={keyFn}/>
-  </div>
- )
-
+  const keyFn = (fruit) => {
+    return fruit.name;
+  };
+  return (
+    <div>
+      <SortableTable data={data} config={config} keyFn={keyFn} />
+    </div>
+  );
 }
 
-export default TablePage
+export default TablePage;
